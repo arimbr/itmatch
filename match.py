21c3e0dde3cdc8cd846b840955ebd6887d1c1c60
ari <arimbr@gmail.com>
2014-01-21 15:55:53 +0200

    mine

--- match.py
+++ match.py
@@ -6,6 +6,6 @@ def generate_users():
 	interests = ["python", "machine learning", "debian", "prezi", "silly hardware"]
 	dic = {"sajjad" : [1,1,0,1,0], "ari" : [1,1,1,1,0], "joakim" : [0,0,1,1,1] }
 	f = open("users.json", "w")
-	dic_json = json.dump(dic, sort_keys=True, indent=4, separators=(',', ': ') )
+	dic_json = json.dumps(dic, sort_keys=True, indent=4, separators=(',', ': ') )
 	f.write(dic_json)
 	return dic
