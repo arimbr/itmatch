1ad7a4c162d53e3b5b61a0213e6a4099e9025e59
ari <arimbr@gmail.com>
2014-03-09 12:24:49 +0200

    add profile view

--- djangoproject/match/views.py
+++ djangoproject/match/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 from match.models import User, Distance, Tag
 
 def home(request):
 
-	users = User.objects.all()[:10]
+	users = User.objects.all()
 
 	return render(request, 'match/home.html', {
 		'users': users,
+	})
+
+def profile(request, user_id):
+	user = get_object_or_404(User, pk=user_id)
+	return render(request, 'match/profile.html', {
+		'user': user,
 	})
