4cc5fc4fe38d3724a141a6e88a7f51d4738df047
ari <arimbr@gmail.com>
2014-03-09 12:14:27 +0200

    fix bug in url regular expression

--- djangoproject/djangoproject/urls.py
+++ djangoproject/djangoproject/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', include('match.urls', namespace="match")),
+    url(r'^', include('match.urls', namespace="match")),
     url(r'^admin/', include(admin.site.urls)),
 )
